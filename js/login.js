ff2555622a49f2dd009979e067ac000d8d75de33
mcittanti <mcittanti@udesa.edu.ar>
2025-11-16 15:34:32 -0300

    Lista de categorias interactiva de la API en el login

---
+++ js/login.js
@@ -0,0 +1,18 @@
+fetch("https://dummyjson.com/products/category-list")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let categoriasMenu = document.querySelector(".listacategorias");
+    let categoriasHtml = "";
+
+    for (let i = 0; i < data.length; i++) {
+        let nombrecategoria = data[i].toUpperCase();
+        let categoriaApi = data[i];
+        categoriasHtml += `<li><a href="./category.html?category=${categoriaApi}">${nombrecategoria}</a></li>`;
+    }
+    categoriasMenu.innerHTML = categoriasHtml;
+  })
+  .catch(function(error) {
+    console.error("Error: ", error);
+  });
