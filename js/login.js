615c6dd4c9d64bf382fea8926b38b4c8cee4d7fb
mperezbertana-ux <mperezbertana@udesa.edu.ar>
2025-11-19 17:53:50 -0300

    login js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,30 @@
-fetch("https://dummyjson.com/products/category-list")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    let categoriasMenu = document.querySelector(".listacategorias");
-    let categoriasHtml = "";
 
-    for (let i = 0; i < data.length; i++) {
-        let nombrecategoria = data[i].toUpperCase();
-        let categoriaApi = data[i];
-        categoriasHtml += `<li><a href="./category.html?category=${categoriaApi}">${nombrecategoria}</a></li>`;
-    }
-    categoriasMenu.innerHTML = categoriasHtml;
-  })
-  .catch(function(error) {
-    console.error("Error: ", error);
-  });
+let formulariolog = document.querySelector(".form-login") 
+let inputemail = document.querySelector(".inputlogin")
+let inputcontra = document.querySelector(".contraseña")
+
+
+formulariolog.addEventListener("submit", function (e) {
+   
+   let email = inputemail.value;
+   let password = inputcontra.value;
+
+   if (email === "" || password === ""){
+      e.preventDefault()
+      alert("Debe completar los campos");
+   } else if (email === ""){
+      e.preventDefault()
+      alert("Debe completar el campo Email")
+   } else if (password === ""){
+      e.preventDefault()
+      alert("Debe completar el campo Contraseña")
+   } else if (password.length > 0 && password.length < 6){
+      e.preventDefault()
+      alert("La contraseña debe tener al menos 6 caracteres")
+   } else {
+      
+      localStorage.setItem("emailUsuario", email);
+      
+   }
+   
+})
