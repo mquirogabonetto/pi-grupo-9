d3621155b08eab0852e408b4e94717b0b1891aa5
mquirogabonetto <mquirogabonetto@udesa.edu.ar>
2025-11-19 16:33:16 -0300

    Cambios search-results.js

--- js/search-results.js
+++ js/search-results.js
@@ -1,20 +1,20 @@
 /*Menú de categorías cargado dinámicamente desde la API */
 fetch("https://dummyjson.com/products/category-list")
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     let categoriasMenu = document.querySelector(".listacategorias");
     let categoriasHtml = "";
 
     for (let i = 0; i < data.length; i++) {
-        let nombrecategoria = data[i].toUpperCase();
-        let categoriaApi = data[i];
-        categoriasHtml += `<li><a href="./category.html?category=${categoriaApi}">${nombrecategoria}</a></li>`;
+      let nombrecategoria = data[i].toUpperCase();
+      let categoriaApi = data[i];
+      categoriasHtml += `<li><a href="./category.html?category=${categoriaApi}">${nombrecategoria}</a></li>`;
     }
     categoriasMenu.innerHTML = categoriasHtml;
   })
-  .catch(function(error) {
+  .catch(function (error) {
     console.error("Error: ", error);
   });
 
@@ -26,26 +26,20 @@ let terminoBuscado = parametro.get("busqueda");
 let tituloPag = document.querySelector(".resultadosencontrados");
 let contenedorProductos = document.querySelector(".categoriaCompleta");
 
-if (terminoBuscado.length < 3){
-  alert('La busqueda debe tener al menos 3 caracteres')
-} else{
-  let url = "https://dummyjson.com/products/search?q=" + terminoBuscado;
-  fetch(url)
-  .then(function(response) {
+let url = "https://dummyjson.com/products/search?q=" + terminoBuscado;
+fetch(url)
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     let productos = data.products;
     let productosHtml = "";
-    console.log("PRODUCTOS:", data.products);
-
-
     if (productos.length === 0) {
-       tituloPag.innerHTML = `<h1>No hay resultados para el término: ${terminoBuscado}</h1>`;
+      tituloPag.innerHTML = `<h1>No hay resultados para el término: ${terminoBuscado}</h1>`;
     } else {
       tituloPag.innerHTML = '<h1 class="busquedaResult" >Resultados de búsqueda para: ' + terminoBuscado + '</h1>';
       for (let i = 0; i < productos.length; i++) {
-        productosHtml += 
+        productosHtml +=
           '<article class="vendidos">' +
           '<img class="foto1" src="' + productos[i].thumbnail + '" alt="' + productos[i].title + '">' +
           '<h2>' + productos[i].title + '</h2>' +
@@ -55,15 +49,15 @@ if (terminoBuscado.length < 3){
           '</article>';
       }
     }
-    
+
     contenedorProductos.innerHTML = productosHtml;
   })
-  .catch(function(error) {
+  .catch(function (error) {
     console.error("Error: ", error);
     tituloPag.innerHTML = '<p>Error al cargar los resultados</p>';
     contenedorProductos.innerHTML = "<p>Error al cargar productos.</p>";
   });
-}
+
 
 
 
