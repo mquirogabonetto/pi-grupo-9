f2cad5c42f15a6c957e300a63df90dd19cb88b0c
mquirogabonetto <mquirogabonetto@udesa.edu.ar>
2025-11-20 23:52:15 -0300

    Categorías en login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,22 @@
+/*Menú de categorías cargado dinámicamente desde la API */
+fetch("https://dummyjson.com/products/category-list")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let categoriasMenu = document.querySelector(".listacategorias");
+    let categoriasHtml = "";
+
+    for (let i = 0; i < data.length; i++) {
+        let nombrecategoria = data[i].toUpperCase();
+        let categoriaApi = data[i];
+        categoriasHtml += `<li><a href="./category.html?category=${categoriaApi}">${nombrecategoria}</a></li>`;
+    }
+    categoriasMenu.innerHTML = categoriasHtml;
+  })
+  .catch(function(error) {
+    console.error("Error: ", error);
+  });
 
 let formulariolog = document.querySelector(".form-login") 
 let inputemail = document.querySelector(".inputlogin")
