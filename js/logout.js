b8099ad34c4ab7b4a065377cea482c99bba7415a
mperezbertana-ux <mperezbertana@udesa.edu.ar>
2025-11-20 22:22:05 -0300

    logout

---
+++ js/logout.js
@@ -0,0 +1,22 @@
+let logout = document.querySelector("#logout")
+
+if(logout){
+    logout.addEventListener("click", function(e) {
+        e.preventDefault()
+
+        localStorage.removeItem("emailUsuario")
+
+        let pagina = document.querySelector(".pagina ul")
+        let listas = pagina.querySelectorAll("li")
+
+        for (let i = 0; i < listas.length; i++) {
+            if (listas[i].querySelector("#logout")){
+                listas[i].innerHTML = `<a href="./login.html">Log In</a>`
+            }
+            
+            if (listas[i.innerText === "Sign Up"]) {
+                listas[i].style.display = "block"
+            }
+        }
+    })
+}
